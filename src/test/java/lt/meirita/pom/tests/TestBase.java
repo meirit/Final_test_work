859e33edf37f608e54ede473b011b285bdebd074
meirit <meiritaserdikauskaite@gmail.com>
2023-11-14 21:00:08 +0200

    Add TestListener class, screenshots

--- src/test/java/lt/meirita/pom/tests/TestBase.java
+++ src/test/java/lt/meirita/pom/tests/TestBase.java
@@ -1,10 +1,16 @@
 package lt.meirita.pom.tests;
 
 import lt.meirita.pom.pages.Common;
+import lt.meirita.pom.utils.TestListener;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+
+@Listeners(TestListener.class)
+
 
 public abstract class TestBase {
+
 
     @BeforeMethod
     public abstract void setUp();
